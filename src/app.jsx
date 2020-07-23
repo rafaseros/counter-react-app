e7caa927083a49231828900cc0e06b5c7dd12ebb
devrafaseros <devrafaseros@gmail.com>
2020-07-22 23:25:37 -0400

    impresion de variables

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const App = () => {
-    
+  const saludo = 'Hola Mundo';
+
   return (
     <>
-      <h1>Hola Mundo</h1>
+      <h1>{saludo}</h1>
       <p>Mi primera aplicacion</p>
     </>
   );
