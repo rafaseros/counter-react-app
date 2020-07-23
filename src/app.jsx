5fb727eb86efac5d65784c714ebfa624594dc452
devrafaseros <devrafaseros@gmail.com>
2020-07-22 23:43:24 -0400

    defaultProps

--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const App = ({ saludo }) => {
+const App = ({ saludo, subtitulo }) => {
   return (
     <>
       <h1>{saludo}</h1>
-      <p>Mi primera aplicacion</p>
+      <p>{subtitulo}</p>
     </>
   );
 };
 
 App.propTypes = {
   saludo: PropTypes.string.isRequired,
+};
+
+App.defaultProps = {
+  subtitulo: 'Soy un subtitulo',
 };
 
 export default App;
