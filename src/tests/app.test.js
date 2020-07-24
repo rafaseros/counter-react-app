c9902c5b8b9ecb5f44ddb4bc5c27b9b940ed78ea
devrafaseros <devrafaseros@gmail.com>
2020-07-23 20:19:36 -0400

    test with enzyme

--- src/tests/app.test.js
+++ src/tests/app.test.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
+import '@testing-library/jest-dom';
 import App from '../app';
 
 describe('Prueba de App.jsx', () => {
-  test('should be show h1 and subtitle', () => {
-    const saludo = 'Hola soy Rafael';
-    const { getByText } = render(<App saludo={saludo} />);
-    expect(getByText(saludo)).toBeInTheDocument();
+  // test('should be show h1 and subtitle', () => {
+  //   // const saludo = 'Hola soy Rafael';
+  //   // const { getByText } = render(<App saludo={saludo} />);
+  //   // expect(getByText(saludo)).toBeInTheDocument();
+  // });
+
+  test('should be show App', () => {
+    const saludo = 'hola soy rafael';
+    const wrapper = shallow(<App saludo={saludo} />);
+
+    expect(wrapper).toMatchSnapshot();
   });
 });
