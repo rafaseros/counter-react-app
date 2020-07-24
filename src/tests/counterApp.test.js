31c639e5ddc5c7594b08506058133dc6fd663d25
devrafaseros <devrafaseros@gmail.com>
2020-07-23 20:59:04 -0400

    Test simulate events

--- src/tests/counterApp.test.js
+++ src/tests/counterApp.test.js
@@ -4,6 +4,10 @@ import '@testing-library/jest-dom';
 import CounterApp from '../counterApp';
 
 describe('Test de counterApp.js', () => {
+  beforeEach(() => {
+    //   Aca se puede reiniciar cualquier constante antes de cada prueba
+  });
+
   test('should be counterApp componenet', () => {
     const value = 10;
     const wrapper = shallow(<CounterApp value={value} />);
@@ -15,5 +19,20 @@ describe('Test de counterApp.js', () => {
     const wrapper = shallow(<CounterApp />);
     const texth2 = wrapper.find('h2').text();
     expect(parseInt(texth2)).toBe(100);
+  });
+
+  test('should increment counter if user click on button +1', () => {
+    const wrapper = shallow(<CounterApp />);
+    wrapper.find('button').at(0).simulate('click');
+    const textCounter = wrapper.find('h2').text();
+
+    expect(textCounter).toBe('101');
+  });
+  test('should decrement counter if user click on button -1', () => {
+    const wrapper = shallow(<CounterApp />);
+    wrapper.find('button').at(2).simulate('click');
+    const textCounter = wrapper.find('h2').text();
+
+    expect(textCounter).toBe('99');
   });
 });
